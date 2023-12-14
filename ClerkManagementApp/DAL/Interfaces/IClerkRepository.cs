6aedcb8f4f5c849197311c87ad69384e78fd445e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-14 10:55:54 +0530

    Add files via upload

---
+++ ClerkManagementApp/DAL/Interfaces/IClerkRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClerkManagementApp.Model;
+
+namespace ClerkManagementApp.DAL.Interfaces
+{
+    public interface IClerkRepository
+    {
+        string GetAll();
+        string Add();
+        string Update();
+        string Delete();
+
+        ClerkModel GetById(string id);
+
+    }
+}
