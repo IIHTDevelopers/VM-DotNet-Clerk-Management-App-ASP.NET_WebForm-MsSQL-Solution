a1dc21837b1a30aa0228e4b633b70b344aaac331
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-24 09:07:34 +0530

    Add files via upload

---
+++ ClerkManagementApp/ClerkManagement.aspx.cs
@@ -0,0 +1,82 @@
+﻿using ClerkManagementApp.DAL;
+using ClerkManagementApp.Model;
+using System;
+using System.Web.UI;
+
+namespace ClerkManagementApp
+{
+    public partial class ClerkManagement : Page
+    {
+        private readonly datalayer _dataLayer;
+        private readonly ClerkManagementApp.DAL.Interfaces.IClerkService _clerkService;
+
+        public ClerkManagement()
+        {
+            _dataLayer = new datalayer();
+            _clerkService = new DAL.Services.ClerkService(new DAL.Services.ClerkRepository(new ClerkManagementApp.DAL.ClerkDbContext()));
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+            }
+        }
+
+        private void BindGridView()
+        {
+            var clerks = _clerkService.GetAll();
+            _dataLayer.fillgridView(clerks, gv);
+        }
+
+        protected void gv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var id = gv.SelectedRow.Cells[1].Text;
+            // Retrieve the clerk details and populate the form
+            var clerk = _clerkService.GetById(id);
+            if (clerk != null)
+            {
+                txtFirstName.Text = clerk.FirstName;
+                txtLastName.Text = clerk.LastName;
+                txtDateOfBirth.Text = clerk.DateOfBirth.ToString("yyyy-MM-dd");
+            }
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            var newClerk = new ClerkModel
+            {
+                FirstName = txtFirstName.Text,
+                LastName = txtLastName.Text,
+                DateOfBirth = DateTime.Parse(txtDateOfBirth.Text)
+            };
+
+            _clerkService.Add();
+            BindGridView();
+        }
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+            string id = gv.SelectedRow.Cells[1].Text.ToString();
+            var existingClerk = _clerkService.GetById(id);
+
+            if (existingClerk != null)
+            {
+                existingClerk.LastName = txtLastName.Text;
+                existingClerk.FirstName = txtFirstName.Text;
+                existingClerk.DateOfBirth = DateTime.Parse(txtDateOfBirth.Text);
+
+                _clerkService.Update();
+                BindGridView();
+            }
+        }
+
+        protected void btndlt_Click(object sender, EventArgs e)
+        {
+            string id = gv.SelectedRow.Cells[1].Text.ToString();
+            _clerkService.Delete();
+            BindGridView();
+        }
+    }
+}
---
+++ ClerkManagementApp/DAL/Services/ClerkRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using ClerkManagementApp.DAL.Interfaces;
+
+namespace ClerkManagementApp.DAL.Services
+{
+    public class ClerkRepository : Interfaces.IClerkRepository
+    {
+        private ClerkDbContext _context;
+
+        public ClerkRepository(ClerkDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.ClerkModel GetById(string id)
+        {
+            return _context.ClerkModels.FirstOrDefault(t => t.Id == int.Parse(id));
+        }
+
+        public string GetAll()
+        {
+            string qry = "select* from ClerkModels";
+            return qry;
+        }
+
+        public string Add()
+        {
+            string qry = "insert into ClerkModels(Title, IsCompleted, DueDate)" +
+                "values('";
+            return qry;
+        }
+
+        public string Update()
+        {
+            var query = "update ClerkModels set Title='";
+            return query;
+        }
+
+        public string Delete()
+        {
+            var query = "delete from ClerkModels where Id='";
+            return query;
+        }
+    }
+}
